80aa822a6d00a90c1e2a0d4b1813fc77ad03be37
Aamer-Qanadilo <aamer_qanadilo@hotmail.com>
2023-05-04 22:58:47 +0300

    Simple Implementation for the .map method

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const array = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+const mapCallback = (value, index, array) => {
+  console.log(value);
+};
+
+Array.prototype.customMap = function (mapCallback = (value, index, array)) {
+  const finalArr = [];
+  for (let i = 0; i < this.length; i++) {
+    finalArr.push(mapCallback(this[i], i, this));
+  }
+  return finalArr;
+};
+
+const arr = array.customMap((value) => value * 2);
+
+console.log(arr);
